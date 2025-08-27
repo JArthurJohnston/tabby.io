ecd049b8924a8d6b49502ad3857afcd33c28cacc
JArthurJohnston <joe.a.johnston@gmail.com>
2025-08-27 08:25:54 -0400

    updated config for deployment to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
-
+const router = createRouter({ routeTree, basepath: '/tabby.io/' })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
