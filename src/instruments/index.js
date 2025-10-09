394282ef7027c462d3c765638fd6a2bce8aafab5
Joe Johnston <joe.a.johnston@gmail.com>
2025-10-09 19:15:46 -0400

    started on instruments menu

---
+++ src/instruments/index.js
@@ -0,0 +1,9 @@
+import naFluteD from './na-flute.json'
+import { pennyWhistleD } from './penny-whistle.json'
+
+export const INSTRUMENTS = [
+    naFluteD, pennyWhistleD
+]
+
+export const NAFlute_D = naFluteD
+export const PennyWhistle_D = pennyWhistleD
